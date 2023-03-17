fd00c74a93beb1d71d137da20f40e14b47b09c4e
krgurevich <kananyan@optonline.net>
2023-03-17 12:09:40 -0400

    deploying

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -44,7 +44,6 @@ const AppNavbar = () => {
         show={showModal}
         onHide={() => setShowModal(false)}
         aria-labelledby='signup-modal'>
-        {/* tab container to do either signup or login component */}
         <Tab.Container defaultActiveKey='login'>
           <Modal.Header closeButton>
             <Modal.Title id='signup-modal'>
--- server/schema/typeDefs.js
+++ server/schema/typeDefs.js
@@ -1,50 +1,50 @@
-const { gql } = require('apollo-server-express');
+const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
-    type User {
-        _id: ID
-        username: String
-        email: String
-        bookCount: Int
-        savedBooks: [Book]
-    }   
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  # Set up an Auth type to handle returning data from a profile creating or user login
+  type Auth {
+    token: ID!
+    user: User
+  }
 
-    type Auth {
-        token: ID!
-        user: User
-    }
+  type Book {
+    bookId: ID
+    title: String
+    authors: [String]
+    description: String
+    image: String
+    link: String
+  }
 
-    type Book {
-        bookId: ID
-        title: String
-        authors: [String]
-        description: String
-        image: String
-        link: String
-    }
+  input BookInput {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
 
-    input BookInput {
-        bookId: ID
-        authors: [String]
-        description: String
-        title: String
-        image: String
-        link: String
-    }
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+  }
 
-    type Query {
-        me: User
-        users: [User]
-        user(username: String!): User
-    }
-
-    type Mutation {
-        login(email: String!, password: String!): Auth
-        addUser(username: String!, email: String!, password: String!): Auth 
-        saveBook(bookData: BookInput!): User
-        removeBook(bookId: ID!): User
-    }
-
+  # Set up mutations to handle creating a profile or logging into a profile and return Auth type
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
 `;
 
 module.exports = typeDefs;
